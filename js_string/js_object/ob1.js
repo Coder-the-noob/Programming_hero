3abfab26b16bed52ca25409ce426f98dfddb1fed
Coder-the-noob <moinmatubbar6@gmail.com>
2025-08-08 01:21:43 +0600

    added the object file

---
+++ js_string/js_object/ob1.js
@@ -0,0 +1,37 @@
+const subject= {
+    name: "Math",
+    teacher: "Mr. Smith",
+    students: ["Alice", "Bob", "Charlie"],
+    nextExam: {
+        date: "2023-10-15",
+        time: "10:00 AM",
+        location: "Room 101"
+    }
+}
+
+console.log(subject);
+
+const bike ={
+    name: "Yamaha",
+    model: "YZF-R15",
+    year: 2022,
+    features: ["ABS", "LED Headlights", "Digital Display"],
+    owner: {
+        name: "John Doe",
+        license: "XYZ12345"
+    }
+
+}
+console.log(bike);
+
+const laptop ={
+    brand: "msi",
+    model: "MSI MODERN 15 A5M",
+    specifications: {
+        processor: "AMD Ryzen 5",
+        ram: "8GB",
+        storage: "512GB SSD"
+    }
+}
+
+console.log(laptop);
