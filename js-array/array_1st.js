5671e7e521d4644f564b44638ed5b2bb5976aaf4
Coder-the-noob <moinmatubbar6@gmail.com>
2025-08-07 23:08:15 +0600

    added the html files in a folder and added a folder js tasks

---
+++ js-array/array_1st.js
@@ -0,0 +1,2 @@
+const numbers =[71 , 72, 73, 74 , 75, 76, 77, 78, 79, 80]; //1st practice
+const fridge =['apples', 'bananas', 'oranges', 'grapes', 'kiwis']; //2nd practice
---
+++ js-array/book_p_3.js
@@ -0,0 +1,12 @@
+const vegetables = ['potato', 'onion', 'brinjal', 'radish', 'carrot', 'tomato', 'ladyfinger', 'gourd', 'cucumber', 'pointed gourd'];//3rd practice
+
+console.log(vegetables.length);
+
+//4th practice
+
+const movies = ['Uthsob', 'Avengers: End game', 'Game of thrones', 'Big Mouse', 'A shop for killers'];
+
+//5th practice
+const cities = ['Dhaka', 'Abu Dhabi', 'Cairo', 'Washington DC', 'London', 'Paris', 'Berlin', 'Kulalumpur', 'Tokyo'];
+
+console.log(cities.length);
