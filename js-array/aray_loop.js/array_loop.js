f19d788884227e9a733a66decf58d20f701534e2
Coder-the-noob <moinmatubbar6@gmail.com>
2025-08-08 16:50:00 +0600

    added some array using loops how to access their elements

---
+++ js-array/aray_loop.js/array_loop.js
@@ -0,0 +1,13 @@
+const fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+for (let i = 0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+const numbers = [12, 45 , 67, 34 , 46, 86, 978];
+
+let k =0;
+while(k < numbers.length){
+    console.log(numbers[k]);
+    k++;
+}
