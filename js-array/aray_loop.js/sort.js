2c1cb6a497808454de19ff0da585913946e4b329
Coder-the-noob <moinmatubbar6@gmail.com>
2025-08-09 19:59:36 +0600

    added a sort array file

---
+++ js-array/aray_loop.js/sort.js
@@ -0,0 +1,12 @@
+const numbers = [5, 3, 8, 1, 2];
+numbers.sort();
+// console.log(numbers);
+
+
+//ascending order
+
+const ages =[15, 24, 17, 100, 45, 60, 30, 37];
+// ages.sort();
+// console.log(ages);
+const sortedAges = ages.sort(function(a,b) {return a- b});
+console.log(sortedAges);
