573daed9be210ea3c2215ec41c6f908ae647751e
Coder-the-noob <moinmatubbar6@gmail.com>
2025-08-10 03:03:29 +0600

    added descending order

--- js-array/aray_loop.js/sort.js
+++ js-array/aray_loop.js/sort.js
@@ -10,3 +10,7 @@ const ages =[15, 24, 17, 100, 45, 60, 30, 37];
 // console.log(ages);
 const sortedAges = ages.sort(function(a,b) {return a- b});
 console.log(sortedAges);
+
+//descending order
+const sortedAgesDesc = ages.sort(function(a , b){ return b - a});
+console.log(sortedAgesDesc);
