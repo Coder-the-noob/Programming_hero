a54c41096e8f9071828b34ce5f88006add601a41
Coder-the-noob <moinmatubbar6@gmail.com>
2025-08-23 23:29:43 +0600

    added event lsitener

---
+++ DOM_EVENT/js/handler.js
@@ -0,0 +1,11 @@
+ document.getElementById('btn-Click').addEventListener('click', function makeClick(){
+        document.getElementById('event-handler').innerText = 'I am Clicked';
+        document.getElementById('event-handler').style.color = 'red';
+    });
+
+document.getElementById('btn-Click-2').addEventListener('click', function makeClick2(){
+    document.getElementById('make-P').innerText = 'Ki Vabchili Manger Po';
+    document.getElementById('make-P').style.backgroundColor = 'aqua';
+
+            
+});
