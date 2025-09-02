7e88241fc50da4004c6a8c2028a6c315e76d96c3
Coder-the-noob <moinmatubbar6@gmail.com>
2025-09-02 23:47:38 +0600

    added the template literals of ES6

---
+++ ES6-Intro/template_strings.js
@@ -0,0 +1,26 @@
+
+//ES6 using template literals
+let name = "Moin";
+let message = `Hello, ${name} !Welcome.`;
+console.log(message);
+
+
+
+/*
+*!-------------------! 
+*/
+
+let a = 5;
+let b = 10;
+let sumMessage = `The sum of ${a} and ${b} is ${a + b}.`;
+console.log(sumMessage);
+
+//Multiline strings
+
+let poem = `This is line 1 
+this is line 2
+this is line 3
+`
+console.log(poem);
+
+
