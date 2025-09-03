6250a3e9f1b706e36fda3bc384bba1672229238a
Coder-the-noob <moinmatubbar6@gmail.com>
2025-09-04 00:37:29 +0600

    added the forEach

---
+++ ES6-Intro/forEach.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(num => {
+    console.log(num * 2);
+});
+
+// Use forEach when you just want to do something with each element (e.g., printing, saving, updating external variables).
