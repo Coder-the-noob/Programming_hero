fa78e5b59d88996f3653ec34ff78bb24beacfe29
Coder-the-noob <moinmatubbar6@gmail.com>
2025-09-16 11:36:49 +0600

    How to convert JSON string to object

--- ES6-Intro/script.js
+++ ES6-Intro/script.js
@@ -13,3 +13,7 @@ console.log(person.name);
 //JSON.stringify() - converts a JavaScript object or value to a JSON string
 const jsonString = JSON.stringify(person);
 console.log(jsonString);
+
+//JSON.parse() - parses a JSON string, constructing the JavaScript value or object described by the string
+const jsonObject = JSON.parse(jsonString);
+console.log(jsonObject);
