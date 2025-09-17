91aefaf7b8b229088d49f49772a3ed4a8ce9b184
Coder-the-noob <moinmatubbar6@gmail.com>
2025-09-17 19:22:02 +0600

    added the fetch display card

--- json/scripts.js
+++ json/scripts.js
@@ -11,15 +11,22 @@ const displayData = (posts) => {
     
     //1.get the container
     const postContainer = document.getElementById('post-container');
+    postContainer.innerHTML = '';
     
     for(const post of posts){
         // console.log(post);
 
         // 2.create HTML element
-        const li = document.createElement('li');
-        li.innerText = post.title;
+        const postCard = document.createElement('div');
+        postCard.innerHTML = `<div class="post-card">
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        </div>`;
 
-        // 3.append child
-        postContainer.appendChild(li);
+        postContainer.append(postCard);
+
+        
     }
 }
+
+loadData();
